fe2049317b187e6ee89b4834bffb7ca19b80360d
bobbysingh <Meghsingh@12345>
2019-07-26 09:06:49 +0530

    Initial commit

---
+++ app/src/main/java/com/stuffshuf/camera/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.stuffshuf.camera
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+if (ActivityCompat.checkSelfPermission(this,
+        android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
+{
+    ActivityCompat.requestPermissions(this,
+        arrayOf(android.Manifest.permission.CAMERA), 123)
+}
+else {
+    textureView.post {
+        startCamera()
+    }
+}
+    }
+
+    private fun startCamera() {
+        //Seet Capture Config
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+        }.build()
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        captureImage.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+
+            imageCapture.takePicture(file, object : ImageCapture.OnImageSavedListener {
+                override fun onImageSaved(file: File) {
+                    Toast.makeText(this@MainActivity,"Photo captured ${file.absolutePath}",Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(useCaseError: ImageCapture.UseCaseError, message: String, cause: Throwable?) {
+                    Toast.makeText(this@MainActivity,"Error $message",Toast.LENGTH_LONG).show()
+                }
+
+            })
+        }
+
+        //set Preview Config
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(1080, 1080))
+            setTargetAspectRatio(Rational(1, 1))
+            setLensFacing(CameraX.LensFacing.BACK)
+        }.build()
+
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = textureView.parent as ViewGroup
+            parent.removeView(textureView)
+            parent.addView(textureView, 0)
+            updateTransform()
+            textureView.surfaceTexture = it.surfaceTexture
+        }
+
+        CameraX.bindToLifecycle(this, preview,imageCapture)
+    }
+
+    private fun updateTransform() {
+        val matrix=Matrix()
+        val centerX= textureView.width/2f
+        val centerY= textureView.width/2f
+
+        val rotationDegress= when(textureView.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+
+        }
+        matrix.postRotate(-rotationDegress.toFloat(), centerX, centerY)
+        textureView.setTransform(matrix)
+    }
+}
+
